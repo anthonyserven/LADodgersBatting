6d578addf19e90a03c51d2d899b57d3757fe0374
Anthony Serven <anthonyserven@gmail.com>
2023-10-26 15:04:26 -0700

    commit

--- dodgerBattingProject.py
+++ dodgerBattingProject.py
@@ -1,24 +1,13 @@
-print("Test")
-
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 from unicodedata import normalize
 
-table_MN = pd.read_html('https://en.wikipedia.org/wiki/Minnesota')
+bettsStats = pd.read_html('https://www.baseball-reference.com/players/b/bettsmo01-bat.shtml') #Referencing baseball-reference.com for stats
 
-print(f'Total tables: {len(table_MN)}')
+print(bettsStats)
 
-table_MN = pd.read_html('https://en.wikipedia.org/wiki/Minnesota', match='United States presidential election results for Minnesota')
-len(table_MN)
+df = bettsStats
 
-print(f'Total tables: {len(table_MN)}')
-
-df = table_MN[0]
-df.head()
 
 # class battingAverage:
-#     def __init__():
-#         print("lalalal")
-#     def mememe():
-#         print("lololo")
